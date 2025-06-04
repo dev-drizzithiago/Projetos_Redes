1b3d305f93a9f597c9766353fc297d6341b9a11f
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-06-04 17:53:16 -0300

    Create main.py

---
+++ pop_up/main.py
@@ -0,0 +1,26 @@
+
+import sys
+from PySide6.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
+from PySide6.QtCore import Qt, QTimer
+
+class PopUp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Aviso Importante")
+        self.setWindowFlags(Qt.WindowStaysOnTopHint | Qt.CustomizeWindowHint | Qt.FramelessWindowHint)
+        self.setFixedSize(300, 150)
+
+        layout = QVBoxLayout()
+        label = QLabel('Esta janela será fechada em 30 segundos.')
+        label.setAlignment(Qt.AlignCenter)
+        layout.addWidget(label)
+        self.setLayout(layout)
+
+        QTimer.singleShot(30000, self.close)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = PopUp()
+    window.show()
+    sys.exit(app.exec())
